3795da0ab7e65df6f68928b27a1963c301f68c8a
Dominic Dreier <dreierdo@gmail.com>
2021-10-20 12:10:49 +0200

    Add: Skeleton Loader

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,34 @@ export default {
     Card,
   },
   setup() {
-    let cryptos = reactive([]);
+    let cryptos = reactive([[]]);
+    let cryptosLimit = 20;
 
     function loadData() {
-      fetch('https://api.coinlore.net/api/tickers/?limit=20')
+      fetch(`https://api.coinlore.net/api/tickers/?limit=${cryptosLimit}`)
         .then(res => {
             return res.json();
           })
           .then(apiData => {
-            cryptos.push(apiData.data);
+            cryptos[0] = apiData.data;
           })
         .catch(error => console.log(error));
     };
 
+    function createSkeletonLoader() {
+      for (let i = 1; i <= cryptosLimit; i++) {
+        cryptos[0].push({});
+      }
+    }
+
     onMounted(() => {
+      createSkeletonLoader();
       loadData();
     });
 
     return {
       cryptos,
+      createSkeletonLoader,
       loadData,
     };
   },
@@ -33,22 +42,23 @@ export default {
 </script>
 
 <template>
-  <div class="bg-blue-200 text-blue-700 mb-10 px-5 md:px-0">
-    <div class="container mx-auto py-20">
-      <h1 class="text-5xl mb-5">
-        CryptoDashboard
-      </h1>
+  <div class="dark:bg-gray-700">
+    <div class="bg-blue-200 text-blue-700 mb-10 px-5 md:px-0 dark:bg-blue-900 dark:text-blue-300">
+      <div class="container mx-auto py-20">
+        <h1 class="text-5xl mb-5">
+          CryptoDashboard
+        </h1>
 
-      <p class="lg:max-w-screen-md">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+        <p class="lg:max-w-screen-md">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+
+    <div class="container mx-auto px-5 md:px-0 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <Card :data="crypto" v-for="crypto in cryptos[0]"/>
     </div>
   </div>
-
-  <div class="container mx-auto px-5 md:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-    <Card :data="crypto" v-for="crypto in cryptos[0]"/>
-  </div>
-
 </template>
 
 <style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,11 @@
 <script>
-import { TrendingUpIcon, TrendingDownIcon } from '@heroicons/vue/outline';
+import { TrendingUpIcon, TrendingDownIcon, MinusIcon } from '@heroicons/vue/outline';
 
 export default {
   components: {
     TrendingUpIcon,
     TrendingDownIcon,
+    MinusIcon,
   },
 
   props: {
@@ -17,20 +18,31 @@ export default {
 </script>
 
 <template>
-  <div class="bg-gray-100 hover:bg-gray-300 p-5 mb-2 rounded-md relative cursor-pointer">
-    <h1 class="text-1xl mb-4">
-      {{data.name}}
-    </h1>
+    <div class="bg-gray-100 hover:bg-gray-300 p-5 mb-2 rounded-md relative cursor-pointer dark:bg-gray-500 dark:text-white dark:hover:bg-gray-600" >
+      <transition name="fade" mode="out-in">
+        <div v-if="data.id">
+          <h1 class="text-1xl mb-4">
+            {{data.name}}
+          </h1>
 
-    <h2 class="text-3xl">
-      $ {{data.price_usd}}
-    </h2>
+          <h2 class="text-3xl">
+            $ {{data.price_usd}}
+          </h2>
 
-    <div class="absolute top-4 right-4">
-      <TrendingUpIcon class="h-6 w-6 inline text-green-500" v-if="Math.sign(Number(data.percent_change_1h)) > 0"/>
-      <TrendingDownIcon class="h-6 w-6 inline text-red-500" v-if="Math.sign(Number(data.percent_change_1h)) < 0"/>
+          <div class="absolute top-4 right-4">
+            <TrendingUpIcon class="h-6 w-6 inline text-green-500" v-if="Math.sign(Number(data.percent_change_1h)) > 0"/>
+            <MinusIcon class="h-6 w-6 inline" v-if="Math.sign(Number(data.percent_change_1h)) === 0"/>
+            <TrendingDownIcon class="h-6 w-6 inline text-red-500" v-if="Math.sign(Number(data.percent_change_1h)) < 0"/>
+          </div>
+        </div>
+
+        <div class="animate-pulse" v-else>
+          <div class="h-5 bg-gray-400 rounded w-2/4 mb-4"></div>
+          <div class="h-10 bg-gray-400 w-3/4 rounded"></div>
+          <div class="h-5 bg-gray-400 rounded w-5 absolute top-5 right-5"></div>
+        </div>
+      </transition>
     </div>
-  </div>
 </template>
 
 <style scoped>
